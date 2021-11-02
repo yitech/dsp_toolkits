023c597e1fc094989de7ac6c30c6db1fafe8854e
yite <yi@lucidinside.com>
2021-11-02 23:37:53 +0800

    finish convert codec

---
+++ convert/__init__.py
@@ -0,0 +1,2 @@
+from .convert_codec import convert_mp4_to_mp3
+from .convert_codec import convert_mp4_to_wav
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import glob
 import fetch
+import convert
 
 if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=LdPjnubLRN0'
-    fetch.download_mp4_from_youtube(url)
+    # url = 'https://www.youtube.com/watch?v=LdPjnubLRN0'
+    # fetch.download_mp4_from_youtube(url)
+    convert.convert_mp4_to_wav("Grandpas_Tea.mp4")
+
