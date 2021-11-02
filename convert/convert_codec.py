a4c3d265ad29a2f1e6201db288ebcb5b8f957e72
yite <yi@lucidinside.com>
2021-11-02 23:36:17 +0800

    finish convert codec

---
+++ convert/convert_codec.py
@@ -0,0 +1,19 @@
+import os
+
+
+def convert_mp4_to_mp3(src, dst=None):
+    if not dst:
+        filename = src.split('.')[0]
+        dst = filename + '.mp3'
+    cmd = "ffmpeg -i {} -b:a 128K -vn {}"
+    os.system(cmd.format(src, dst))
+    return
+
+
+def convert_mp4_to_wav(src, dst=None):
+    if not dst:
+        filename = src.split('.')[0]
+        dst = filename + '.wav'
+    cmd = "ffmpeg -i {} -b:a 128K -vn {}"
+    os.system(cmd.format(src, dst))
+    return
