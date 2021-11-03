b67c708f4c3c11fb0e238348a5dd923b8b37da2a
yite <yi@lucidinside.com>
2021-11-03 23:35:06 +0800

    detail log

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # STT
     st = stt.STT()
     for wav_clip in data['segment'].keys():
-        print(st.to_text(wav_clip))
+        print(wav_clip, st.to_text(wav_clip))
 
     # save files
     with open(file='data/data.json', mode='w+') as jfile:
