e5f8cd12ff6dc30396601d06c87df917f9aec974
yite <yi@lucidinside.com>
2021-11-03 10:56:12 +0800

    correct import class

--- seg/speech_segmenter.py
+++ seg/speech_segmenter.py
@@ -1,9 +1,2 @@
-import inaSpeechSegmenter
+from inaSpeechSegmenter import Segmenter
 
-
-class Segmenter(inaSpeechSegmenter.segmenter):
-    def __init__(self):
-        super(Segmenter, self).__init__()
-
-    def segment(self, wav_file):
-        return super(Segmenter, self).__call__(wav_file)
