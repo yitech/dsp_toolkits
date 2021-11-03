9a6fac655cd6451eafe140329d9f53adf454a693
yite <yi@lucidinside.com>
2021-11-03 14:27:34 +0800

    update main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import glob
 import fetch
-from seg import Segmenter
 import convert
 
 if __name__ == '__main__':
@@ -8,9 +7,7 @@ if __name__ == '__main__':
     fetch.download_mp4_from_youtube(urls[0], 'data')
     files = glob.glob('data/*.wav')
     wav_file = convert.convert_mp4_to_wav(files[0])
-    s = Segmenter()
-    print(wav_file)
-    print(s(wav_file))
+
 
 
 
