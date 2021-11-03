1a47f360145771fcd336313b175a860eab2f0ff4
yite <yi@lucidinside.com>
2021-11-03 10:50:43 +0800

    wav only

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import convert
 if __name__ == '__main__':
     urls = ['https://www.youtube.com/watch?v=R0Uq8Eg39R0']
     fetch.download_mp4_from_youtube(urls[0], 'data')
-    files = glob.glob('data/*')
+    files = glob.glob('data/*.wav')
     wav_file = convert.convert_mp4_to_wav(files[0])
     s = Segmenter()
     print(wav_file)
