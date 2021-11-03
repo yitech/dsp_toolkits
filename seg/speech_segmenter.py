d3c048720c4ca125b2820cd56b72d56af629347f
yite <yi@lucidinside.com>
2021-11-03 09:23:40 +0800

    add method segment

--- seg/speech_segmenter.py
+++ seg/speech_segmenter.py
@@ -5,4 +5,5 @@ class Segmenter(inaSpeechSegmenter.segmenter):
     def __init__(self):
         super(Segmenter, self).__init__()
 
-
+    def segment(self, wav_file):
+        return super(Segmenter, self).__call__(wav_file)
