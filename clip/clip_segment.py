c29768e42647184186112cea61585062596f140d
yite <yi@lucidinside.com>
2021-11-04 13:44:23 +0800

    end time -> duration

--- clip/clip_segment.py
+++ clip/clip_segment.py
@@ -3,10 +3,11 @@ import time
 
 
 def clip_segment(wav_file, start_seconds, end_seconds, dst):
-    cmd = 'ffmpeg -ss {start_time} -i """{input}""" -to {end_time} -c copy """{output}"""'
+    cmd = 'ffmpeg -ss {start_time} -i """{input}""" -to {duration} -c copy """{output}"""'
     start_ms = start_seconds - int(start_seconds)
     start_time = time.strftime('%H:%M:%S', time.gmtime(start_seconds)) + f".{str(start_ms).split('.')[-1]}"
-    end_ms = end_seconds - int(end_seconds)
-    end_time = time.strftime('%H:%M:%S', time.gmtime(end_seconds)) + f".{str(end_ms).split('.')[-1]}"
-    os.system(cmd.format(start_time=start_time, input=wav_file, end_time=end_time, output=dst))
+    duration_seconds = end_seconds - start_seconds
+    duration_ms = duration_seconds - int(duration_seconds)
+    duration_time = time.strftime('%H:%M:%S', time.gmtime(duration_seconds)) + f".{str(duration_ms).split('.')[-1]}"
+    os.system(cmd.format(start_time=start_time, input=wav_file, duration=duration_time, output=dst))
     return dst
